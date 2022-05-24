79105399fe702c67149f422b3c7026eb3cd9ad48
Gustavo <gustavoliberato77@gmail.com>
2022-05-23 21:28:28 -0300

    first commit

---
+++ imparepar.js
@@ -0,0 +1,16 @@
+function Imparepar() {
+let numero = parseFloat(prompt("Digite um numero"));
+
+if (numero % 2 == 0 ){
+    resultado = "Número é PAR"
+    alert("Número Par")
+}
+else{
+    resultado = "Número é IMPAR"
+    alert ("Número Impar")
+}
+
+console.log (numero)
+
+}
+
